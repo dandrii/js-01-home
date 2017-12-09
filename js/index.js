be355eee0544f258a68cc5894705ab77e5da5cfe
Andrii Doronin <doronin.andrii.lv@gmail.com>
2017-12-09 11:57:20 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -2,27 +2,27 @@ let a = parseFloat(prompt(`Enter value - a`));
 let b = parseFloat(prompt(`Enter value - b`));
 let c = parseFloat(prompt(`Enter value - c`));
 
-if (a !== 0) {
+function sq_roots(a, b, c) {
 
-    function sq_roots(a, b, c) {
+    let d = (Math.pow(b, 2)) - (4 * a * c);
 
-        let d = (Math.pow(b, 2)) - (4 * a * c);
-
-        if (d > 0) {
-            let x = ((-b) + Math.sqrt(d)) / (2 * a);
-            let y = ((-b) - Math.sqrt(d)) / (2 * a);
-            return `x = ${x}<br>y = ${y}`;
-        }
-
-        if (d == 0) {
-            let x = ((-b) / 2 * a);
-            return `x = y = ${x}`;
-        }
-
-        if (d < 0) {
-            return `infinite roots`
-        }
+    if (d > 0) {
+        let x = ((-b) + Math.sqrt(d)) / (2 * a);
+        let y = ((-b) - Math.sqrt(d)) / (2 * a);
+        return `x = ${x}<br>y = ${y}`;
     }
+
+    if (d == 0) {
+        let x = ((-b) / 2 * a);
+        return `x = y = ${x}`;
+    }
+
+    if (d < 0) {
+        return `infinite roots`
+    }
+}
+
+if (a !== 0) {
     document.write(sq_roots(a, b, c));
 }
 
